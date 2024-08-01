14c727bed4dad2c484c30aa2f9d050ad66f7ad54
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-01 16:30:00 -0300

    feat: Adiciona Link Botão para criar um produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,12 +1,16 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Product } from '../../shared/interfaces/product';
 import { ProductsService } from '../../shared/services/products.service';
 import { CardComponent } from './components/card/card.component';
 
+/* ANGULAR MATERIAL */
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
