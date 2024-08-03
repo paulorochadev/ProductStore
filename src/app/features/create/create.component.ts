536cdf2ca20f43d381287ddc21fda7c50a9e06e6
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-03 09:59:05 -0300

    feat: Utiliza o Component back-to-list em Criação e Edição de produto

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { BackToListComponent } from '../../shared/components/back-to-list/back-to-list.component';
 import { FormComponent } from '../../shared/components/form/form.component';
 import { Product } from '../../shared/interfaces/product.interface';
 import { ProductsService } from '../../shared/services/products.service';
@@ -10,7 +11,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent, BackToListComponent],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
